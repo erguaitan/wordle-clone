225466fe9181dfb418946f151407935b4d804fb4
erguaitan <albertodiazpacheco09@gmail.com>
2025-03-27 04:28:46 +0100

    * added light and dark theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import React, { use, useEffect, useState } from 'react'
 import { obtainNewSolution } from './lib/lib';
 import Wordle from './components/Wordle';
+import SwapTheme from './components/SwapTheme';
 
 const App = () => {
   const [solution, setSolution] = useState(null);
+
+  const currentTheme = document.documentElement.getAttribute("data-theme");
+  console.log("Theme " + currentTheme);
+  document.documentElement.setAttribute("data-theme", "dark");
+  console.log("Theme " + currentTheme);
 
   useEffect(() => {
     const solution = obtainNewSolution();
@@ -12,7 +18,8 @@ const App = () => {
 
   return (
     <div className='App'>
-      <h1 className='text-2xl text-[#333] py-4 mb-7.5 border-b-2 border-b-[#eee] font-extrabold'>Wordle</h1>
+      <h1>Wordle Clone</h1>
+      <SwapTheme className={"absolute top-0 right-0 m-4"} size='size-7 m-1' />
       {solution && <Wordle solution={solution} />}
     </div>
   )
